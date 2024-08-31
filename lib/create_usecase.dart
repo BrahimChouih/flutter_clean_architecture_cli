7783cfa3f03ec6e6ad27d7017a2861822eff82fb
brahimchouih <brahim26chouih@gmail.com>
2024-08-31 15:15:45 +0100

    fix a bug

--- lib/create_usecase.dart
+++ lib/create_usecase.dart
@@ -43,10 +43,10 @@ class FlutterCleanArchitectureCLIUseCase {
       usecaseTemplate(featureName, usecaseName),
     );
 
-    await FileHelper.addToTheBeginOfFile(
-      repositoriesDomainDir,
-      "import '../usecases/${usecaseName.snakeCase}_usecase.dart';",
-    );
+    // await FileHelper.addToTheBeginOfFile(
+    //   repositoriesDomainDir,
+    //   "import '../usecases/${usecaseName.snakeCase}_usecase.dart';",
+    // );
 
     await FileHelper.addToTheEndOfClass(
       repositoriesDomainDir,
